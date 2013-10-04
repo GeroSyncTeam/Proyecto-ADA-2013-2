26fac29e4173b6d3ba275f209096ce2dc6c84804
Gero <Gero@Gero>
2013-10-04 14:05:39 -0500

    carpetas con los sprietes de aviones

---
+++ src/clases/Avion.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+import java.awt.Rectangle;
+
+/**
+ *
+ * @author Gero
+ */
+public class Avion extends Rectangle{
+    
+}
